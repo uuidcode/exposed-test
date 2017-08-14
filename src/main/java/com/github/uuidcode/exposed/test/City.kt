0e78d01a5730ee9b6bdbbb074c027c8a6d35c27a
uuidcode <uuidcode@gmail.com>
2017-08-14 16:28:26 +0900

    Initial commit

---
+++ src/main/java/com/github/uuidcode/exposed/test/City.kt
@@ -0,0 +1,41 @@
+package main.java.com.github.uuidcode.exposed.test
+
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.SchemaUtils.create
+import org.jetbrains.exposed.sql.transactions.transaction
+
+object City : Table("city") {
+    val id = long("id").autoIncrement().primaryKey()
+    val name = varchar("name", 50)
+    val displayName = varchar("display_name", 50)
+}
+
+fun main(args: Array<String>) {
+    val url = System.getProperty("url")
+    val user = System.getProperty("user")
+    val password = System.getProperty("password")
+
+    Database.connect(url = url,
+            driver = "com.mysql.jdbc.Driver",
+            user = user,
+            password = password)
+
+    transaction {
+        create(City)
+
+        val city = City.insert {
+            it[name] = "test"
+            it[displayName] = "display_test"
+        }
+
+        City.update({
+            City.id eq 1
+        }) {
+            it[displayName] = "display_test_111"
+        }
+
+        City.deleteWhere {
+            City.id eq city[City.id]
+        }
+    }
+}
